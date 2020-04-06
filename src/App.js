082328e06c0e3a17df06c7e5d93b5171d167c9e8
e-slick <33139655+e-slick@users.noreply.github.com>
2020-04-05 20:01:17 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import MovieHeader from './components/movieheader';
+import Authentication from './components/authentication';
+import {HashRouter,Route} from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './stores/store'
+
+function App() {
+  return (
+      <div className="App">
+        <Provider store={store}>
+          <HashRouter>
+            <div>
+              <MovieHeader />
+              <Route exact path="/" render={()=><div />}/>
+              <Route path="/signin" render={()=><Authentication />}/>
+            </div>
+          </HashRouter>
+        </Provider>
+      </div>
+  );
+}
+
+export default App;
